cf4f2cb17612accff9b47c5ed9407c2ee2d049f3
Shawon Barua <shawon.cse.ku@gmail.com>
2021-09-26 02:20:06 +0600

    url from file code changed

--- src/test/java/utils/BrowserSetting.java
+++ src/test/java/utils/BrowserSetting.java
@@ -2,20 +2,22 @@ package utils;
 
 import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.WebDriver;
+import dataReader.ConfigFileReader;
 
 import static variables.UrlLink.*;
 
 public class BrowserSetting {
 	//public static WebDriver driver = null;
+	ConfigFileReader configFileReader;
 	public WebDriver BrowserSettings() {
-		
+		configFileReader= new ConfigFileReader();
 		WebdriverSettings wd = new WebdriverSettings();
 		WebDriver driver = wd.settings();
 		driver.manage().deleteAllCookies();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
-		driver.get(BASE_URL);
+		driver.get(configFileReader.getApplicationUrl());
 		return driver;
 	}
 
